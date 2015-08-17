4302eb832a6fd0c4a23698fcca617d993bca4e89
Emolotow <neugebam@hochschule-trier.de>
2015-08-17 23:38:08 +0200

    first
    try

---
+++ Numenera/Darrumar.mm
@@ -0,0 +1,101 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#cccccc" COLOR="#000000" CREATED="1422986106996" ID="ID_1681735966" MODIFIED="1422987738575" TEXT="Darrumar">
+<font BOLD="true" NAME="Open Sans" SIZE="24"/>
+<node BACKGROUND_COLOR="#ccccff" COLOR="#0066ff" CREATED="1422986125053" FOLDED="true" ID="ID_367172768" MODIFIED="1422993176252" POSITION="right" TEXT="Slums">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="20"/>
+<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422986820310" ID="ID_1706657421" MODIFIED="1422987738576" TEXT="Haus von Jensen">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422986838725" ID="ID_1866746798" MODIFIED="1422987738576" TEXT="Schn&#xfc;rhalle">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422987865490" ID="ID_1996834777" MODIFIED="1422988925130" TEXT="g&#xfc;ldene Nacht">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422988934403" ID="ID_948978515" MODIFIED="1422988959169" TEXT="S&#xfc;dturm">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ccccff" COLOR="#0066ff" CREATED="1422986138866" FOLDED="true" ID="ID_6151534" MODIFIED="1422993166595" POSITION="left" TEXT="Oberstadt">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="20"/>
+<node BACKGROUND_COLOR="#ffffff" COLOR="#333333" CREATED="1422986342106" ID="ID_583620013" MODIFIED="1422990328711" STYLE="bubble" TEXT="Hier kommt die Party erst hinein, nachdem sie vom Verwalter am Versammlungsplatz Einlass bekommen haben. Die Eingangstore werden von gut bezahlten Wachen besch&#xfc;tzt, die ihren Job sehr ernst nehmen.&#xa;Die Oberstadt hat sichtlich mehr &#xf6;ffentliche Mittel zur Verf&#xfc;gung als der Rest der Stadt. Der Boden ist gepflastert. Die gro&#xdf;en H&#xe4;user stehen weiter auseinander und &#xfc;berall wo Platz ist sind B&#xe4;ume und diverse Gr&#xfc;nanlagen zwischen den pomp&#xf6;sen Geb&#xe4;uden.&#xa;Es patrouillieren auch mehr Wachen auf den Stra&#xdf;en. Ein paar H&#xe4;user, die mit der hohen Stadtmauer verwachsen sind, haben nochmal eine restaurierte Rest&#xfc;berdachung die von der Mauer sich teilweise bis in die Mitte des Viertels zieht.">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422986883099" ID="ID_1382694392" MODIFIED="1422987738591" TEXT="Nordturm">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422990349412" ID="ID_1077760535" MODIFIED="1422990363055" TEXT="Sommerresidenz der Kaiserin">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ccccff" COLOR="#0066ff" CREATED="1422986234098" FOLDED="true" ID="ID_1655904065" MODIFIED="1422993179232" POSITION="right" TEXT="Markt der D&#xfc;fte">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="20"/>
+<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422986234098" ID="ID_1954008319" MODIFIED="1422987990290" TEXT="Schmied">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422987996256" ID="ID_1327693293" MODIFIED="1422988053345" TEXT="Platz der Gew&#xfc;rz- und Kr&#xe4;uterh&#xe4;ndler">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422988058235" ID="ID_1832986676" MODIFIED="1422988122922" TEXT="Basar der Parf&#xfc;meure">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422988739496" ID="ID_1650524829" MODIFIED="1422988766898" TEXT="Ostturm">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422988976493" ID="ID_1111626143" MODIFIED="1422989093766" TEXT="Karawanenposten">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ccccff" COLOR="#0066ff" CREATED="1422986234098" FOLDED="true" ID="ID_1764597944" MODIFIED="1422993170090" POSITION="left" TEXT="Tempelviertel">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="20"/>
+<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422986668749" ID="ID_797389811" MODIFIED="1422987738591" TEXT="Theater">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422986682670" FOLDED="true" ID="ID_231131807" MODIFIED="1422993153952" TEXT="Basilika der Wahrheit">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node BACKGROUND_COLOR="#ffffff" COLOR="#333333" CREATED="1422989132835" ID="ID_1245394975" MODIFIED="1422989894336" STYLE="bubble" TEXT="Die Basilika steht im Zentrum der Stadt. Sie ist aus hellen Marmor geschaffen und hat nur kleine Fenster, die wenig Licht ins Innere vor lassen.&#xa;In dem Prunkgeb&#xe4;ude stehen in Reihen abgegriffene dunkle Holzb&#xe4;nke die unregelm&#xe4;&#xdf;ig gepolstert sind. Das innere ist mit dunkleren gemusterten Marmor verkleidet. &#xdc;berall stehen kleine Alt&#xe4;re und Fugen in der Wand in die Ansammlungen von Kerzen gestellt wurden.&#xa;Im vorderen Schiff des Innenraums ist ein schwerer quaderf&#xf6;rmiger Steinaltar, der durch einen Mechanismus nach oben fahren kann und einen Eingang zum Fahrstuhl verbirgt, der in die Unterstadt f&#xe4;hrt.">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422986719682" ID="ID_942117825" MODIFIED="1422987738591" TEXT="Kapelle der Ernteg&#xf6;ttin">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422989108671" ID="ID_1444665868" MODIFIED="1422989114436" TEXT="Westturm">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ccccff" COLOR="#0066ff" CREATED="1422986234098" FOLDED="true" ID="ID_195050366" MODIFIED="1422993182305" POSITION="right" TEXT="Versammlungsplatz">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="20"/>
+<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422986234098" ID="ID_1110401094" MODIFIED="1422988822419" TEXT="Bibliothek">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422988781664" ID="ID_272778712" MODIFIED="1422988812310" TEXT="Stadtverwaltung">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422988831163" ID="ID_1986185841" MODIFIED="1422988868626" TEXT="Gildenhaus der Maschinenbauer">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ccccff" COLOR="#0066ff" CREATED="1422990578246" FOLDED="true" ID="ID_591346224" MODIFIED="1422993173085" POSITION="left" TEXT="Unterstadt">
+<font NAME="SansSerif" SIZE="20"/>
+<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422990593611" ID="ID_131070109" MODIFIED="1422990604479" TEXT="Kontrolzentrum">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
---
+++ Numenera/Politische Strukturen.mm
@@ -0,0 +1,66 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#cccccc" COLOR="#000000" CREATED="1422991637990" ID="ID_561871107" MODIFIED="1422991676457" TEXT="Politische Strukturen">
+<font BOLD="true" NAME="Open Sans" SIZE="24"/>
+<node BACKGROUND_COLOR="#cccccc" COLOR="#000000" CREATED="1422991662258" HGAP="-276" ID="ID_718523953" MODIFIED="1422991968640" POSITION="right" TEXT="Der Orden der Wahrheit" VSHIFT="-73">
+<font BOLD="true" NAME="Open Sans" SIZE="24"/>
+<node BACKGROUND_COLOR="#ccccff" COLOR="#0066ff" CREATED="1422991682217" ID="ID_1285829756" MODIFIED="1422997651249" TEXT="Anh&#xe4;nger">
+<font NAME="SansSerif" SIZE="20"/>
+<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422992743147" FOLDED="true" ID="ID_1090502413" MODIFIED="1422997627918" TEXT="Sakrale">
+<font NAME="SansSerif" SIZE="16"/>
+<node BACKGROUND_COLOR="#ffffff" COLOR="#333333" CREATED="1422993470380" ID="ID_1871283019" MODIFIED="1422996261816" STYLE="bubble" TEXT="Sind immer mit magischen F&#xe4;higkeiten ausgestattet. Haben immer mehrere Cypher dabei um sich gegen m&#xf6;gliche Angreifer zu verteidigen. Sie sind geschult auf Numeneras und k&#xf6;nnen die meisten alten Gegenst&#xe4;nde in k&#xfc;rzester Zeit identifizieren - sie sind aber ebenfalls darauf aus, diese Gegenst&#xe4;nde f&#xfc;r den Orden zu Gewinnen. Bei ihnen bekommt man sehr gut seine Cypher los, aber es wird schwierig welche zu erhalten. Au&#xdf;er man arbeitet direkt in deren Auftrag.&#xa;Priester, Bisch&#xf6;fe, Kleriker und M&#xf6;nche der Wahrheit haben immer entweder Cypher, die ihnen erlauben zu schweben, die sie unsichtbar machen, oder die ihnen R&#xfc;stung verleihen.&#xa;Seltener Verdoppler oder Phasendistruptoren.">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422992756001" FOLDED="true" ID="ID_864496151" MODIFIED="1422997628388" TEXT="Agenten">
+<font NAME="SansSerif" SIZE="16"/>
+<node BACKGROUND_COLOR="#ffffff" COLOR="#333333" CREATED="1422994774861" ID="ID_923341708" MODIFIED="1422996478266" STYLE="bubble" TEXT="Sind sehr gut ger&#xfc;stete Spezialeinheiten. Sie sind meist undercover und versuchen nicht allzuviel Aufmerksamkeit auf sich zu ziehen. K&#xf6;nnen aber fast immer durch Ausweise, Oddities oder Tattoos identifiziert werden.&#xa;Wenn sie einen offenen Kampf suchen, werden sie meist von Sakralen begleitet bzw haben Nanos in der Gruppe. Ein Sondereinsatzkommando wird aber immer den leisesten Weg vorziehen um ihren Auftrag zu erf&#xfc;llen.&#xa;Ein Mitglied des Agentenstabs ist sehr pflichtbewusst und dem Orden als oberer Teil von dessen Hierarchie verschrieben."/>
+</node>
+<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422992840764" FOLDED="true" ID="ID_1263289618" MODIFIED="1422997628798" TEXT="Akolythen">
+<font NAME="SansSerif" SIZE="16"/>
+<node CREATED="1422995169721" ID="ID_1504001429" MODIFIED="1422996261806" STYLE="bubble" TEXT="Sind die Arbeiter und Angestellten des Ordens. Sie haben selten Cypher dabei und sind fast nie in den Kenntnissen der Numenera geschult.&#xa;Sie erledigen alle Arbeiten und sind sowohl in der Verwaltung als auch in der Instanthaltung des Ordens t&#xe4;tig. Da sie als Vorgesetzten immer mindestens einen Sakralen haben, sind sie meist in einem guten Verh&#xe4;ltnis mit diesem. Auch wenn der Orden mit abnehmender Hierarchie immer weniger von sich Preis gibt - die Akolythen wissen meist nur &#xfc;ber die Dinge bescheid,  die zur Aus&#xfc;bung ihrer individuellen T&#xe4;tigkeiten notwendig sind."/>
+</node>
+<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422992848962" FOLDED="true" ID="ID_1052365279" MODIFIED="1422997629378" TEXT="Anh&#xe4;nger">
+<font NAME="SansSerif" SIZE="16"/>
+<node CREATED="1422995573943" ID="ID_1194527819" MODIFIED="1422996261806" STYLE="bubble" TEXT="Sind ganz normale B&#xfc;rger, die -unabh&#xe4;ngig von ihrer Kaste- dem Orden positiv zugetan sind. Sie sind der &#xdc;berzeugung, dass die Lehren die der Orden verbreitet die richtigen sind. Sie vertrauen in die Wissenschaft und den ihnen auferlegten Dogmen. Diese k&#xf6;nnen sich je nach Standort und lokalem Oberhaupt unterscheiden.&#xa;Wenn sie es sich leisten k&#xf6;nnen, spenden sie dem Orden Geld, Cypher, Oddities und Artefakte - nat&#xfc;rlich nur wenn es ihnen gelingt welche aufzutreiben."/>
+</node>
+<node BACKGROUND_COLOR="#ffffff" COLOR="#333333" CREATED="1422992901698" ID="ID_1854389575" MODIFIED="1422996695326" STYLE="bubble" TEXT="Die meisten Ordensmitglieder der Wahrheit sind orange gewandet. Viele haben nach Bedarf technische Mittel, zur Verteidigung, oder um ihre spezifischen Aufgaben zu erledigen.&#xa;Die h&#xf6;heren Anh&#xe4;nger sind meist aufgrund der zugesicherten Position im Dienst. Akolythen sind entweder Angestellte oder sozial engagiert.&#xa;Die meisten, die -wenn &#xfc;berhaupt- religi&#xf6;s verblendet sind, finden sich unter den Anh&#xe4;ngern.">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ccccff" COLOR="#0066ff" CREATED="1422991704339" ID="ID_1617530435" MODIFIED="1422991752669" TEXT="Oberhaupt">
+<font NAME="SansSerif" SIZE="20"/>
+<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422992653117" ID="ID_417479225" MODIFIED="1422992667813" TEXT="Der Bernsteinpapst">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ccccff" COLOR="#0066ff" CREATED="1422991722824" FOLDED="true" ID="ID_1733800405" MODIFIED="1422997638249" TEXT="Ziele">
+<font NAME="SansSerif" SIZE="20"/>
+<node CREATED="1422996774444" ID="ID_425109512" MODIFIED="1422997094108" TEXT="Auf der einen Seite will der Orden seine Machtstruktur ausbauen. Der quasi-kirchliche Zustand wird dabei als Mittel der Wahl genutzt. So bekommen die sakralen &#xc4;mter weltweit Kunde von neuen Entdeckungen, gefundenen oder wiedererwachten Artefakten.&#xa;Passiert also irgendwo etwas, was auch nur im geringsten etwas mit der Vergangenheit und/oder den Numenera zu tun hat, wird der Orden nicht lange auf sich warten lassen."/>
+<node CREATED="1422997106266" ID="ID_1549323263" MODIFIED="1422997596119" TEXT="Auf der anderen Seite hat der Orden, durch sein gro&#xdf;es Netzwerk und die entsprechenden L&#xe4;ndereien auch einige soziale Einrichtungen aus dem Boden wachsen lassen.&#xa;In gr&#xf6;&#xdf;eren St&#xe4;dten wird sich immer ein Waisenhaus, eine oder mehrere Ordensschulen und ein Krankenhaus finden lassen. Hier werden sehr h&#xe4;ufig eine gro&#xdf;e Anzahl an Akolythen besch&#xe4;ftigt, aber Sakrale und Agenten sind eher weniger zu finden.&#xa;Nat&#xfc;rlich k&#xf6;nnen auch solche Einrichtungen missbraucht werden. In den Waisenh&#xe4;usern und Ordensschulen werden Kinder zu zuk&#xfc;nftigen &#xc4;mtern erzogen und ausgesiebt. Die Krankenh&#xe4;user tarnen geheime Labore in denen unaussprechliches mit Menschen passiert, die keiner vermissen wird."/>
+</node>
+<node BACKGROUND_COLOR="#ccccff" COLOR="#0066ff" CREATED="1422991789644" ID="ID_1230505369" MODIFIED="1422991799513" TEXT="Einrichtungen">
+<font NAME="SansSerif" SIZE="20"/>
+</node>
+<node BACKGROUND_COLOR="#ccccff" COLOR="#0066ff" CREATED="1422992716785" ID="ID_231735467" MODIFIED="1422992737899" TEXT="Gr&#xfc;ndung">
+<font NAME="SansSerif" SIZE="20"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#cccccc" COLOR="#000000" CREATED="1422991736199" HGAP="6" ID="ID_1931657028" MODIFIED="1422991971640" POSITION="left" TEXT="Die Konvergenz" VSHIFT="49">
+<font BOLD="true" NAME="Open Sans" SIZE="24"/>
+<node BACKGROUND_COLOR="#ccccff" COLOR="#0066ff" CREATED="1422991808325" ID="ID_1050016315" MODIFIED="1422991902673" TEXT="Anh&#xe4;nger">
+<font NAME="SansSerif" SIZE="20"/>
+</node>
+<node BACKGROUND_COLOR="#ccccff" COLOR="#0066ff" CREATED="1422991876553" ID="ID_1458022606" MODIFIED="1422991902674" TEXT="Oberhaupt">
+<font NAME="SansSerif" SIZE="20"/>
+</node>
+<node BACKGROUND_COLOR="#ccccff" COLOR="#0066ff" CREATED="1422991887192" HGAP="34" ID="ID_59748488" MODIFIED="1422991955386" TEXT="Ziele" VSHIFT="1">
+<font NAME="SansSerif" SIZE="20"/>
+</node>
+<node BACKGROUND_COLOR="#ccccff" COLOR="#0066ff" CREATED="1422991907786" ID="ID_1810855259" MODIFIED="1422991926210" TEXT="Einrichtungen">
+<font NAME="SansSerif" SIZE="20"/>
+</node>
+<node CREATED="1422992729887" ID="ID_634922702" MODIFIED="1422992733336" TEXT="Gr&#xfc;ndung"/>
+</node>
+</node>
+</map>
