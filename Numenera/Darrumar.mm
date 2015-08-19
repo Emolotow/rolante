137aca7f9228bad8bf0c7b2552484c1d72d6e3f0
Emolotow <neugebam@hochschule-trier.de>
2015-08-19 13:53:06 +0200

    Dateien erstellt
    Gitignore abgeändert, so dass keine Sicherheitskopien mit hochgeladen
    werden.

--- Numenera/Darrumar.mm
+++ Numenera/Darrumar.mm
@@ -1,99 +1,180 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node BACKGROUND_COLOR="#cccccc" COLOR="#000000" CREATED="1422986106996" ID="ID_1681735966" MODIFIED="1422987738575" TEXT="Darrumar">
-<font BOLD="true" NAME="Open Sans" SIZE="24"/>
-<node BACKGROUND_COLOR="#ccccff" COLOR="#0066ff" CREATED="1422986125053" FOLDED="true" ID="ID_367172768" MODIFIED="1422993176252" POSITION="right" TEXT="Slums">
+<node BACKGROUND_COLOR="#999999" COLOR="#000000" CREATED="1422986106996" ID="ID_1681735966" MODIFIED="1439976399840" TEXT="Darrumar">
+<edge COLOR="#000000" WIDTH="8"/>
+<font BOLD="true" NAME="SansSerif" SIZE="20"/>
+<node BACKGROUND_COLOR="#99ccff" COLOR="#0033ff" CREATED="1422986125053" ID="ID_367172768" MODIFIED="1439976390592" POSITION="right" TEXT="Slums">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="SansSerif" SIZE="20"/>
-<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422986820310" ID="ID_1706657421" MODIFIED="1422987738576" TEXT="Haus von Jensen">
-<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node BACKGROUND_COLOR="#66ff66" COLOR="#00b439" CREATED="1422986820310" ID="ID_1706657421" MODIFIED="1439976390594" TEXT="Haus von Jensen">
+<edge COLOR="#00b439" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1439899756707" ID="ID_285525177" MODIFIED="1439900080414" TEXT="">
+<edge COLOR="#990000" WIDTH="1"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#66ff66" COLOR="#00b439" CREATED="1422986838725" ID="ID_1866746798" MODIFIED="1439976390598" TEXT="Schn&#xfc;rhalle">
+<edge COLOR="#00b439" STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 </node>
-<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422986838725" ID="ID_1866746798" MODIFIED="1422987738576" TEXT="Schn&#xfc;rhalle">
-<edge STYLE="bezier" WIDTH="thin"/>
+<node BACKGROUND_COLOR="#66ff66" COLOR="#00b439" CREATED="1422987865490" ID="ID_1996834777" MODIFIED="1439983596787" TEXT="g&#xfc;ldene Nacht">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        <p>
+          <span style="font-weight: normal">Der heruntergekommene Sandstein-Holzbau hat einen direkten Zugang zum Gastraum. Die Tische stehen weit genug auseinander, dass man ungest&#246;rt Tuscheln kann.</span><span style="font-weight: normal"><br /></span><span style="font-weight: normal">Am Tag findet sich dort h&#246;chstens eine Alkoholleiche vom Vortag ein.</span><b><br /></b><span style="font-weight: normal">In der g&#252;ldenen Nacht angekommen sehen sie einen kleinen hutzeligen Mann in der Ecke des Aufenthaltsraumes auf dessen Tisch ein Sonnenstrahl von drau&#223;en hereinscheint. Er hat sch&#252;tteres, ungepflegtes Haar und eine Sehhilfe mit einer Art Lupenvorrichtung auf der spitzen Nase.</span><span style="font-weight: normal"><br /></span><span style="font-weight: normal">Nach dem Vorzeigen der Zettel werden die Spieler einer Untersuchung unterzogen.</span><span style="font-weight: normal"><br /></span><span style="font-weight: normal">Der Mann &#246;ffnet sein Lederetui und holt ein Rasiermesser heraus. Dies setzt er auf den Ellbogen der Charaktere an und f&#228;hrt einmal gerade &#252;ber den gesamten Unterarm. Anschlie&#223;end wird das gesammelte Material auf einem Leinentuch ausgebreitet. Der Mann h&#228;lt es zur n&#228;heren Betrachtung in die Sonne.</span><span style="font-weight: normal"><br /></span><span style="font-weight: normal">Dann l&#228;sst er alle Spieler wissen, dass unweit westlich von hier ein verrammelte</span><span style="font-weight: normal">r</span><span style="font-weight: normal">&#160;</span><span style="font-weight: normal">Verschlag</span><span style="font-weight: normal">&#160;</span><span style="font-weight: normal">steht. An diesem ist ein X mit Kreide gemalt. Sie sollen an die T&#252;r klopfen und m&#246;glichst vorsichtig fl&#252;stern: 'Der Tod ist gr&#252;n' -&gt;Jensens Absteige</span><span style="font-weight: normal"><br /></span><span style="font-weight: normal"><br /></span><i><span style="font-weight: normal">Solange die Spieler f&#252;r Jensen arbeiten, bekommen sie in der G&#252;ldenen Nacht einen gratis Schlafplatz und eine Mahlzeit am Tag.</span><span style="font-weight: normal"><br /></span><span style="font-weight: normal">Abends f&#252;llt sich der Gastraum mit allerlei G&#228;sten und Huren. Auch findet man hier einen Musikanten und die K&#246;chin.</span><span style="font-weight: normal"><br /></span><span style="font-weight: normal">Sollten die Spieler den Stadtwachen auffallen, besteht eine Chance, dass diese hier ihre Gesichter erkennen und Unruhe stiften. Auch gibt es eine 10% Chance, dass an einem Abend eine Kneipenschl&#228;gerei stattfindet.</span></i>
+        </p>
+        <p style="line-height: 100%; margin-bottom: 0cm">
+          
+        </p>
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#00b439" STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 </node>
-<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422987865490" ID="ID_1996834777" MODIFIED="1422988925130" TEXT="g&#xfc;ldene Nacht">
+<node BACKGROUND_COLOR="#66ff66" COLOR="#00b439" CREATED="1422988934403" ID="ID_948978515" MODIFIED="1439976390605" TEXT="S&#xfc;dturm">
+<edge COLOR="#00b439" STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 </node>
-<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422988934403" ID="ID_948978515" MODIFIED="1422988959169" TEXT="S&#xfc;dturm">
-<font NAME="SansSerif" SIZE="16"/>
+<node BACKGROUND_COLOR="#ffcccc" COLOR="#990000" CREATED="1439901655781" ID="ID_1382484910" MODIFIED="1439976609047" TEXT="Marek">
+<edge COLOR="#ff3333" STYLE="linear" WIDTH="2"/>
+<font BOLD="true" NAME="SansSerif" SIZE="20"/>
 </node>
 </node>
-<node BACKGROUND_COLOR="#ccccff" COLOR="#0066ff" CREATED="1422986138866" FOLDED="true" ID="ID_6151534" MODIFIED="1422993166595" POSITION="left" TEXT="Oberstadt">
+<node BACKGROUND_COLOR="#99ccff" COLOR="#0033ff" CREATED="1422986138866" ID="ID_6151534" MODIFIED="1439976390615" POSITION="left" TEXT="Oberstadt">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Hier kommt die Party erst hinein, nachdem sie vom Verwalter am Versammlungsplatz Einlass bekommen haben.</b>
+    </p>
+    <p>
+      Die Eingangstore werden von gut bezahlten Wachen besch&#252;tzt, die ihren Job sehr ernst nehmen.
+    </p>
+    <p>
+      Die Oberstadt hat sichtlich mehr &#246;ffentliche Mittel zur Verf&#252;gung als der Rest der Stadt. Der Boden ist gepflastert. Die gro&#223;en H&#228;user stehen weiter auseinander und &#252;berall wo Platz ist sind B&#228;ume und diverse Gr&#252;nanlagen zwischen den pomp&#246;sen Geb&#228;uden.
+    </p>
+    <p>
+      Es patrouillieren auch mehr Wachen auf den Stra&#223;en. Ein paar H&#228;user, die mit der hohen Stadtmauer verwachsen sind, haben nochmal eine restaurierte Rest&#252;berdachung die von der Mauer sich teilweise bis in die Mitte des Viertels zieht.
+    </p>
+  </body>
+</html></richcontent>
 <edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node BACKGROUND_COLOR="#66ff66" COLOR="#00b439" CREATED="1422986883099" ID="ID_1382694392" MODIFIED="1439976390617" TEXT="Nordturm">
+<edge COLOR="#00b439" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node BACKGROUND_COLOR="#66ff66" COLOR="#00b439" CREATED="1422990349412" ID="ID_1077760535" MODIFIED="1439976457373" TEXT="Winterdomizil der Kaiserin">
+<edge COLOR="#00b439" WIDTH="2"/>
 <font NAME="SansSerif" SIZE="20"/>
-<node BACKGROUND_COLOR="#ffffff" COLOR="#333333" CREATED="1422986342106" ID="ID_583620013" MODIFIED="1422990328711" STYLE="bubble" TEXT="Hier kommt die Party erst hinein, nachdem sie vom Verwalter am Versammlungsplatz Einlass bekommen haben. Die Eingangstore werden von gut bezahlten Wachen besch&#xfc;tzt, die ihren Job sehr ernst nehmen.&#xa;Die Oberstadt hat sichtlich mehr &#xf6;ffentliche Mittel zur Verf&#xfc;gung als der Rest der Stadt. Der Boden ist gepflastert. Die gro&#xdf;en H&#xe4;user stehen weiter auseinander und &#xfc;berall wo Platz ist sind B&#xe4;ume und diverse Gr&#xfc;nanlagen zwischen den pomp&#xf6;sen Geb&#xe4;uden.&#xa;Es patrouillieren auch mehr Wachen auf den Stra&#xdf;en. Ein paar H&#xe4;user, die mit der hohen Stadtmauer verwachsen sind, haben nochmal eine restaurierte Rest&#xfc;berdachung die von der Mauer sich teilweise bis in die Mitte des Viertels zieht.">
-<edge STYLE="bezier" WIDTH="thin"/>
-<font NAME="SansSerif" SIZE="12"/>
 </node>
-<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422986883099" ID="ID_1382694392" MODIFIED="1422987738591" TEXT="Nordturm">
-<edge STYLE="bezier" WIDTH="thin"/>
-<font NAME="SansSerif" SIZE="16"/>
-</node>
-<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422990349412" ID="ID_1077760535" MODIFIED="1422990363055" TEXT="Sommerresidenz der Kaiserin">
-<font NAME="SansSerif" SIZE="16"/>
+<node BACKGROUND_COLOR="#66ff66" COLOR="#00b439" CREATED="1439976359242" ID="ID_423275979" MODIFIED="1439976449197" TEXT="Haus des Regenten">
+<edge COLOR="#00b439" WIDTH="2"/>
+<font NAME="SansSerif" SIZE="20"/>
 </node>
 </node>
-<node BACKGROUND_COLOR="#ccccff" COLOR="#0066ff" CREATED="1422986234098" FOLDED="true" ID="ID_1655904065" MODIFIED="1422993179232" POSITION="right" TEXT="Markt der D&#xfc;fte">
+<node BACKGROUND_COLOR="#99ccff" COLOR="#0033ff" CREATED="1422986234098" ID="ID_1655904065" MODIFIED="1439976390625" POSITION="right" TEXT="Markt der D&#xfc;fte">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="SansSerif" SIZE="20"/>
-<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422986234098" ID="ID_1954008319" MODIFIED="1422987990290" TEXT="Schmied">
-<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node BACKGROUND_COLOR="#66ff66" COLOR="#00b439" CREATED="1422986234098" ID="ID_1954008319" MODIFIED="1439976623090" STYLE="fork" TEXT="Schmied">
+<edge COLOR="#00b439" STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 </node>
-<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422987996256" ID="ID_1327693293" MODIFIED="1422988053345" TEXT="Platz der Gew&#xfc;rz- und Kr&#xe4;uterh&#xe4;ndler">
+<node BACKGROUND_COLOR="#66ff66" COLOR="#00b439" CREATED="1422987996256" ID="ID_1327693293" MODIFIED="1439976390629" TEXT="Platz der Gew&#xfc;rz- und Kr&#xe4;uterh&#xe4;ndler">
+<edge COLOR="#00b439" STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 </node>
-<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422988058235" ID="ID_1832986676" MODIFIED="1422988122922" TEXT="Basar der Parf&#xfc;meure">
+<node BACKGROUND_COLOR="#66ff66" COLOR="#00b439" CREATED="1422988058235" ID="ID_1832986676" MODIFIED="1439976390631" TEXT="Basar der Parf&#xfc;meure">
+<edge COLOR="#00b439" STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 </node>
-<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422988739496" ID="ID_1650524829" MODIFIED="1422988766898" TEXT="Ostturm">
+<node BACKGROUND_COLOR="#66ff66" COLOR="#00b439" CREATED="1422988739496" ID="ID_1650524829" MODIFIED="1439976390632" TEXT="Ostturm">
+<edge COLOR="#00b439" STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 </node>
-<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422988976493" ID="ID_1111626143" MODIFIED="1422989093766" TEXT="Karawanenposten">
+<node BACKGROUND_COLOR="#66ff66" COLOR="#00b439" CREATED="1422988976493" ID="ID_1111626143" MODIFIED="1439976390634" TEXT="Karawanenposten">
+<edge COLOR="#00b439" STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 </node>
 </node>
-<node BACKGROUND_COLOR="#ccccff" COLOR="#0066ff" CREATED="1422986234098" FOLDED="true" ID="ID_1764597944" MODIFIED="1422993170090" POSITION="left" TEXT="Tempelviertel">
+<node BACKGROUND_COLOR="#99ccff" COLOR="#0033ff" CREATED="1422986234098" ID="ID_1764597944" MODIFIED="1439976390638" POSITION="left" TEXT="Tempelviertel">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      D
+    </p>
+  </body>
+</html></richcontent>
 <edge STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="SansSerif" SIZE="20"/>
-<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422986668749" ID="ID_797389811" MODIFIED="1422987738591" TEXT="Theater">
-<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node BACKGROUND_COLOR="#66ff66" COLOR="#00b439" CREATED="1422986668749" ID="ID_797389811" MODIFIED="1439976390640" TEXT="Theater">
+<edge COLOR="#00b439" STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 </node>
-<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422986682670" FOLDED="true" ID="ID_231131807" MODIFIED="1422993153952" TEXT="Basilika der Wahrheit">
-<edge STYLE="bezier" WIDTH="thin"/>
+<node BACKGROUND_COLOR="#66ff66" COLOR="#00b439" CREATED="1422986682670" ID="ID_231131807" MODIFIED="1439976390642" TEXT="Basilika der Wahrheit">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Die Basilika steht im Zentrum der Stadt. Sie ist aus hellen Marmor geschaffen und hat nur kleine Fenster, die wenig Licht ins Innere vor lassen.
+    </p>
+    <p>
+      In dem Prunkgeb&#228;ude stehen in Reihen abgegriffene dunkle Holzb&#228;nke die unregelm&#228;&#223;ig gepolstert sind. Das innere ist mit dunkleren gemusterten Marmor verkleidet. &#220;berall stehen kleine Alt&#228;re und Fugen in der Wand in die Ansammlungen von Kerzen gestellt wurden.
+    </p>
+    <p>
+      Im vorderen Schiff des Innenraums ist ein schwerer quaderf&#246;rmiger Steinaltar, der durch einen Mechanismus nach oben fahren kann und einen Eingang zum Fahrstuhl verbirgt, der in die Unterstadt f&#228;hrt.
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#00b439" STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
-<node BACKGROUND_COLOR="#ffffff" COLOR="#333333" CREATED="1422989132835" ID="ID_1245394975" MODIFIED="1422989894336" STYLE="bubble" TEXT="Die Basilika steht im Zentrum der Stadt. Sie ist aus hellen Marmor geschaffen und hat nur kleine Fenster, die wenig Licht ins Innere vor lassen.&#xa;In dem Prunkgeb&#xe4;ude stehen in Reihen abgegriffene dunkle Holzb&#xe4;nke die unregelm&#xe4;&#xdf;ig gepolstert sind. Das innere ist mit dunkleren gemusterten Marmor verkleidet. &#xdc;berall stehen kleine Alt&#xe4;re und Fugen in der Wand in die Ansammlungen von Kerzen gestellt wurden.&#xa;Im vorderen Schiff des Innenraums ist ein schwerer quaderf&#xf6;rmiger Steinaltar, der durch einen Mechanismus nach oben fahren kann und einen Eingang zum Fahrstuhl verbirgt, der in die Unterstadt f&#xe4;hrt.">
-<font NAME="SansSerif" SIZE="12"/>
 </node>
-</node>
-<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422986719682" ID="ID_942117825" MODIFIED="1422987738591" TEXT="Kapelle der Ernteg&#xf6;ttin">
-<edge STYLE="bezier" WIDTH="thin"/>
+<node BACKGROUND_COLOR="#66ff66" COLOR="#00b439" CREATED="1422986719682" ID="ID_942117825" MODIFIED="1439976390644" TEXT="Kapelle der Ernteg&#xf6;ttin">
+<edge COLOR="#00b439" STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 </node>
-<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422989108671" ID="ID_1444665868" MODIFIED="1422989114436" TEXT="Westturm">
+<node BACKGROUND_COLOR="#66ff66" COLOR="#00b439" CREATED="1422989108671" ID="ID_1444665868" MODIFIED="1439976390645" TEXT="Westturm">
+<edge COLOR="#00b439" STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 </node>
 </node>
-<node BACKGROUND_COLOR="#ccccff" COLOR="#0066ff" CREATED="1422986234098" FOLDED="true" ID="ID_195050366" MODIFIED="1422993182305" POSITION="right" TEXT="Versammlungsplatz">
+<node BACKGROUND_COLOR="#99ccff" COLOR="#0033ff" CREATED="1422986234098" ID="ID_195050366" MODIFIED="1439976390648" POSITION="right" TEXT="Hauptstra&#xdf;e">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="SansSerif" SIZE="20"/>
-<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422986234098" ID="ID_1110401094" MODIFIED="1422988822419" TEXT="Bibliothek">
-<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node BACKGROUND_COLOR="#66ff66" COLOR="#00b439" CREATED="1422986234098" ID="ID_1110401094" MODIFIED="1439976390656" TEXT="Bibliothek">
+<edge COLOR="#00b439" STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 </node>
-<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422988781664" ID="ID_272778712" MODIFIED="1422988812310" TEXT="Stadtverwaltung">
+<node BACKGROUND_COLOR="#66ff66" COLOR="#00b439" CREATED="1422988781664" ID="ID_272778712" MODIFIED="1439976390659" TEXT="Stadtverwaltung">
+<edge COLOR="#00b439" STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 </node>
-<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422988831163" ID="ID_1986185841" MODIFIED="1422988868626" TEXT="Gildenhaus der Maschinenbauer">
+<node BACKGROUND_COLOR="#66ff66" COLOR="#00b439" CREATED="1422988831163" ID="ID_1986185841" MODIFIED="1439976390661" TEXT="Gildenhaus der Maschinenbauer">
+<edge COLOR="#00b439" STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 </node>
 </node>
-<node BACKGROUND_COLOR="#ccccff" COLOR="#0066ff" CREATED="1422990578246" FOLDED="true" ID="ID_591346224" MODIFIED="1422993173085" POSITION="left" TEXT="Unterstadt">
-<font NAME="SansSerif" SIZE="20"/>
-<node BACKGROUND_COLOR="#ccffcc" COLOR="#999900" CREATED="1422990593611" ID="ID_131070109" MODIFIED="1422990604479" TEXT="Kontrolzentrum">
+<node BACKGROUND_COLOR="#99ccff" COLOR="#0033ff" CREATED="1422990578246" ID="ID_591346224" MODIFIED="1439976390665" POSITION="left" TEXT="Unterstadt">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node BACKGROUND_COLOR="#66ff66" COLOR="#00b439" CREATED="1422990593611" HGAP="44" ID="ID_131070109" MODIFIED="1439976390667" TEXT="Kontrollzentrum" VSHIFT="49">
+<edge COLOR="#00b439" STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 </node>
 </node>
